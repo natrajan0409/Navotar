377239e8bac1953a50b4243b31346b03ef938183
natrajan0409 <50269661+natrajan0409@users.noreply.github.com>
2019-12-23 08:24:57 +0530

    validation
    changes updated

--- src/main/java/navotarTestcases/TestScinarious.java
+++ src/main/java/navotarTestcases/TestScinarious.java
@@ -16,7 +16,12 @@ public class TestScinarious {
     
 	public static void   URL() throws InterruptedException {
 			
-		System.setProperty("Webdriver.chrome.driver","E:\\Selinium\\DRIVERS\\chromedriver.exe");
+		String path = System.getProperty("user.dir");   // return project folder path
+
+		String driverpath = path + "\\WEBDRIVER\\chromedriver.exe";   // return driver folder path 
+
+		System.setProperty("webdriver.chrome.driver",driverpath );
+		//System.setProperty("Webdriver.chrome.driver","E:\\Selinium\\DRIVERS\\chromedriver.exe");
 		 driver= new ChromeDriver();
 		String baseUrl = "http://67.43.12.211/NavotarReservation/?clientid=321&mode=qa&AspxAutoDetectCookieSupport=1#/?lang=en%20%3Chttp:%2F%2F67.43.12.211%2FNavotarReservation%2F%3Fclientid&mode=qa&AspxAutoDetectCookieSupport=1#%2F%3Flang=en%3E";
 	        driver.get(baseUrl);
@@ -43,38 +48,73 @@ public class TestScinarious {
     	System.out.println("TestScenar1");
     	Thread.sleep(5000);
         
+    	//Vehicletype
+    	 Boolean dropdownPresent = driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[1]/div[2]/select")).isEnabled();
+    	 
+    	 if(dropdownPresent==true)
+         {
+    	
         Select Vehicletype = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[1]/div[2]/select")));
 Vehicletype.selectByValue("20305");
+System.out.println("Dropdown is Enabled");
+         }
+    	 
+    		 else{
+    	            System.out.println("Dropdown is Disabled");
+    	        }
 
 //PickUp Date
 WebElement PickDate = driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[2]/div[1]/p/input"));
+
+if(PickDate.isEnabled())
+{
 PickDate.clear();
 PickDate.sendKeys("01-January-2020");
+System.out.println("Pick Date is enabled ");
+}
+else
+{
+	System.out.println("Pick Date is disabled ");	
+}
 
 //PickUp Time
 Select PickupTime = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[2]/div[2]/select")));
 PickupTime.selectByValue("23");
 
+
 //Drop OFf Date
 WebElement DropDate = driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[3]/div[1]/p/input"));
+
+if(DropDate.isEnabled())
+{
 DropDate.clear();
 DropDate.sendKeys(" 03-January-2020");
-
+System.out.println("DropDate is enabled ");
+}
+    
+else
+{
+	System.out.println("DropDate is disabled ");	
+}
 //DropOff Time
 
 Select DropTime = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[3]/div[2]/select")));
 DropTime.selectByValue("25");
+
+
+
 
 //PickUp Location Selection
 Select PickLocation = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[4]/div[1]/select")));
 PickLocation.selectByValue("28592");
 
 //Different Drop Off Location Check Box
-if ( !driver.findElement(By.name("test") ).isSelected() )
+if ( !driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[5]/div/input")).isSelected());
 {
 	Thread.sleep(1000);
- driver.findElement(By.name("test")).click();
+	driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[5]/div/input")).click();
 }
+
 
 //Drop Off Location
 Select DropLocation = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[4]/div[2]/select")));
@@ -82,14 +122,38 @@ DropLocation.selectByValue("28593");
 
 //Search 
 
-driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[7]/div[2]/button/translate/span")).click();
+WebElement Element2=driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[7]/div[2]/button/translate/span"));
+
+if( Element2.isEnabled())
+{
+	Element2.click();
+}
+else
+{	
+System.out.println("Search button disable");
+}
+
+
 
 //Radio Selection
-
-
-
-
 new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("(//label[contains(@class,'css-label radGroup1')])[3]"))).click();
+
+
+String URR3="http://67.43.12.211/NavotarReservation//Wizard#/step1";
+String CurrentUR2= driver.getCurrentUrl();
+
+
+if(CurrentUR2.equals(URR3))
+{
+	System.out.println("MVP chekc box page URL is Match");
+	}
+else
+
+{
+	driver.close();
+}
+
+
 
 //Next Button
 
@@ -100,7 +164,7 @@ if( element.isEnabled())
 	element.click();
 }else
 {	
-System.out.println("Search button disable");
+System.out.println("Next button disable");
 }
 driver.quit();
 Thread.sleep(5000);
@@ -113,43 +177,92 @@ Thread.sleep(5000);
     	System.out.println("TestScenar2");	
     	Thread.sleep(5000);
     	
-    	 Select Vehicletype = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[1]/div[2]/select")));
-    	 Vehicletype.selectByValue("20305");
-
-    	 //PickUp Date
+    	
+    	
     	 
+    	//Vehicletype
+    	 Boolean dropdownPresent2 = driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[1]/div[2]/select")).isEnabled();
+    	 
+    	 if(dropdownPresent2==true)
+         {
+    	
+        Select Vehicletype = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[1]/div[2]/select")));
+Vehicletype.selectByValue("20305");
+System.out.println("Dropdown is Enabled");
+         }
+    	 
+    		 else{
+    	            System.out.println("Dropdown is Disabled");
+    	        }
+    	 //PickUp Date         	 
     	 WebElement PickDate = driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[2]/div[1]/p/input"));
+    	 if(PickDate.isEnabled())
+    	 {
     	 PickDate.clear();
     	 PickDate.sendKeys("01-January-2020");
+    	 System.out.println("Pick Date is enabled ");
+    	 }
+    	 else
+    	 {
+    	 	System.out.println("Pick Date is disabled ");	
+    	 }
+    	
 
     	 //PickUp Time
     	 Select PickupTime = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[2]/div[2]/select")));
     	 PickupTime.selectByValue("42");
-
+           
+    	
     	 //Drop OFf Date
     	 WebElement DropDate = driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[3]/div[1]/p/input"));
-    	 DropDate.clear();
-    	 DropDate.sendKeys(" 03-January-2020");
+
+if(DropDate.isEnabled())
+{
+DropDate.clear();
+DropDate.sendKeys(" 03-January-2020");
+System.out.println("DropDate is enabled ");
+}
+    
+else
+{
+	System.out.println("DropDate is disabled ");	
+}
+    	
 
     	 //DropOff Time
 
     	 Select DropTime = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[3]/div[2]/select")));
     	 DropTime.selectByValue("42");
-
+   
     	 //PickUp Location Selection
     	 Select PickLocation = new Select(driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[4]/div[1]/select")));
     	 PickLocation.selectByValue("28592");
+    
     	 
     	//Different Drop Off Location Check Box
-    	 if ( driver.findElement(By.name("test") ).isSelected() )
+    	 if ( driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[5]/div/input")).isSelected());
     	 {
     	 	Thread.sleep(1000);
-    	  driver.findElement(By.name("test")).click();
+    	 
+    	 	System.out.println("Different Drop Off  Check BoxUnchecked ");
     	 }
 
-    	 driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[7]/div[2]/button")).click();
-    	  	
-    	 System.out.println("end");
-////    	 driver.close();
+    	 ////search Button
+    	 WebElement Element2=driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div[7]/div[2]/button/translate/span"));
+
+    	 if( Element2.isEnabled())
+    	 {
+    	 	Element2.click();
+    	 }
+    	 else
+    	 {	
+    	 System.out.println("Search button disable");
+    	 }
+    	 
+    	 driver.quit();
+    	 Thread.sleep(5000);
     }
+    	  	
+    	 
+
 }
